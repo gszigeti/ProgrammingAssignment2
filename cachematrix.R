5b75cf0a7866e85ce8252353be8ae91245ea20e7
Gabor Szigeti <g.szigeti@criteo.com>
2016-11-06 15:48:14 +0100

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions (makeCacheMatrix and cacheSolve) will create a special
+## Matrix object, that can be filled with values, calculates the mean, and caches it. 
+## By using the second function, the mean will be inversed.
 
-## Write a short comment describing this function
+## Creates a special Matrix object, and calculates & caches it's mean value
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setmeaninverse <- function(inverse) inv <<- inverse
+  getmeaninverse <- function() inv
+  list(set = set,
+       get = get,
+       setmeaninverse = setmeaninverse,
+       getmeaninverse = getmeaninverse)
 }
 
-
-## Write a short comment describing this function
+## This function inverses the mean received from the makeCacheMatrix above.
+## If nothing changed in the Matrix, the cached version will be fetched.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getmeaninverse()
+        if(!is.null(inv)){
+          message("using cached data")
+          return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data)
+        x$setmeaninverse(inv)
+        inv
 }
